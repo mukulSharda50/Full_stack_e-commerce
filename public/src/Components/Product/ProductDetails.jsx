979ec73a0bc0107a3f1701f53fcd81e0b1854874
Mukul <mukulsharda5@gmail.com>
2023-07-30 22:55:29 +0530

    implemented add_to_cart, product page in frontedn

--- public/src/Components/Product/ProductDetails.jsx
+++ public/src/Components/Product/ProductDetails.jsx
@@ -1,10 +1,32 @@
 import { Carousel } from 'react-responsive-carousel';
-import styled from "styled-components";
 
-const ProductDetails = () => {
+const ProductDetails = ({ prodId, title, img, price, qty, category }) => {
+    const HandleAddToCart = (e) => {
+        const user_id = localStorage.getItem('user_id');
+        const prodDetails = {
+            prodId: prodId,
+            prodTitle: title,
+            prodImg: img,
+            prodPrice: price,
+            prodQty: qty,
+            prodCategory: category,
+        }
+        fetch('http://localhost:5000/add_to_cart', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(prodDetails)
+        }).then(resp => {
+            console.log(resp);
+            return resp.json();
+        }).then(
+            console.log("success")
+        )
+    }
     return (
-        <Container>
-            <ImgContainer>
+        <div className='flex-col mx-2 mt-3 gap-[1rem] h-[50vh]'>
+            <div>
                 <Carousel
                     emulateTouch={true}
                     showStatus={false}
@@ -12,71 +34,24 @@ const ProductDetails = () => {
                     autoFocus={true}
                     showThumbs={false}
                 >
-                    <Img src="https://random.imagecdn.app/500/261" alt="random" loading='lazy' />
-                    <Img src="https://random.imagecdn.app/500/261" alt="shoes" />
-                    <Img src="https://random.imagecdn.app/500/260" alt="random" loading='lazy' />
-                    <Img src="https://random.imagecdn.app/500/262" alt="random" loading='lazy' />
+                    <img src="https://random.imagecdn.app/500/261" alt="random" loading='lazy' />
+                    <img src="https://random.imagecdn.app/500/261" alt="shoes" />
+                    <img src="https://random.imagecdn.app/500/260" alt="random" loading='lazy' />
+                    <img src="https://random.imagecdn.app/500/262" alt="random" loading='lazy' />
                 </Carousel>
-            </ImgContainer>
-            <ContentContainer>
-                <ProductTitle>Product title</ProductTitle>
-                <ProductDescription>Product Description:</ProductDescription>
-                <ProductDescriptionContent>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum autem doloremque provident itaque? Ullam, amet numquam? Rerum animi minima enim ratione, nisi libero numquam ipsam, pariatur optio unde suscipit dolore!Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum autem doloremque provident itaque? Ullam, amet numquam? Rerum animi minima enim ratione, nisi libero numquam ipsam, pariatur optio unde suscipit dolore!Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum autem doloremque provident itaque? Ullam, amet numquam? Rerum animi minima enim ratione, nisi libero numquam ipsam, pariatur optio unde suscipit dolore!Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum autem doloremque provident itaque? Ullam, amet numquam? Rerum animi minima enim ratione, nisi libero numquam ipsam, pariatur optio unde suscipit dolore!Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum autem doloremque provident itaque? Ullam, amet numquam? Rerum animi minima enim ratione, nisi libero numquam ipsam, pariatur optio unde suscipit dolore!</ProductDescriptionContent>
-                <h2>Product Price</h2>
-                <BuyContainer>
-                    <Button>Add to cart</Button>
-                    <Button>Add to wishlist</Button>
-                </BuyContainer>
-            </ContentContainer>
-        </Container>
+            </div>
+            <div className='flex-col h-[100%] text-slate-900'>
+                <h3 className='uppercase tracking-wider'>cars</h3>
+                <p className='mt-2'> Description:</p>
+                <p className='line-clamp-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum autem doloremque provident itaque? Ullam, amet numquam? Rerum animi minima enim ratione, nisi libero numquam ipsam, pariatur optio unde suscipit dolore!Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum autem doloremque provident itaque? Ullam, amet numquam? Rerum animi minima enim ratione, nisi libero numquam ipsam, pariatur optio unde suscipit dolore!Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum autem doloremque provident itaque? Ullam, amet numquam? Rerum animi minima enim ratione, nisi libero numquam ipsam, pariatur optio unde suscipit dolore!Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum autem doloremque provident itaque? Ullam, amet numquam? Rerum animi minima enim ratione, nisi libero numquam ipsam, pariatur optio unde suscipit dolore!Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum autem doloremque provident itaque? Ullam, amet numquam? Rerum animi minima enim ratione, nisi libero numquam ipsam, pariatur optio unde suscipit dolore!</p>
+                <h4 className='mt-2 mb-3'>Price: Rs.200</h4>
+                <div className='w-[100%] flex justify-evenly items-center'>
+                    <button className='bg-rose-500 text-slate-100 px-2 rounded-md' onClick={(e) => HandleAddToCart(e)}>Add to cart</button>
+                    <button className='ml-5 bg-rose-500 text-slate-100 px-2 rounded-md'>Add to wishlist</button>
+                </div>
+            </div>
+        </div>
     )
 }
-const Container = styled.div`
-    display: flex;
-    align-items: flex-start;
-    justify-content: space-evenly;
-    margin-inline: 2rem;
-    margin-top: 3rem;
-    column-gap: 1rem;
-`;
-
-const ImgContainer = styled.div`
-    max-width: 600px;
-    min-height: 200px;
-    box-shadow: 3px 10px 10px rgba(0, 0, 0, 0.5);
-`;
-const Img = styled.img`
-    width: 100%;
-    height: 100%;
-    object-fit: contain;
-`;
-const ContentContainer = styled.div`
-    // display: flex;
-    // flex-direction: column;
-    // align-items: flex-start;
-    // border: 2px solid red;
-    max-width: 500px;
-`;
-const ProductTitle = styled.h3`
-    text-transform: uppercase;
-    letter-spacing: 1px;
-`;
-const ProductDescription = styled.h3`
-    text-transform: uppercase;
-    letter-spacing: 1px;
-    font-weight: 400;
-    font-size: 1rem;
-`;
-const ProductDescriptionContent = styled.p`
-    word-spacing: 1px;
-    font-weight: 400;
-    font-size: 0.9rem;
-    width: 100%;
-`;
-const BuyContainer = styled.div``;
-
-const Button = styled.button`
-`;
-
 
 export default ProductDetails
